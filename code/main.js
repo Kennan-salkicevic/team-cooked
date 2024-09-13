61e92c56ce899338d591223534cd8b6b1fe8059b
YouriDenisse <302676718@student.rocmondriaan.nl>
2024-09-13 09:42:11 +0200

    we hebben de random nummer generator werkend gemaakt in console

--- code/main.js
+++ code/main.js
@@ -1,7 +1,12 @@
-// function getRandomInt(max) {
-//     return Math.floor(Math.random() * 6) + 1;
-//   }
+function getRandomInt(max) {
+    return Math.floor(Math.random() * 6) + 1;
+  }
   
-//   console.log(getRandomInt(3));
+  console.log(getRandomInt(3));
 
-// de basis voor een random getaal van 1 tot 6 
+// de basis voor een random getal van 1 tot 6 
+
+let = getRandomInt
+//we made the variable for random number between 1 and 6
+
+
